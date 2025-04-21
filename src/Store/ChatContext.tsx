1ce92ac35bb638d297bcc30c41daf3dde4a040b2
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-04-21 11:52:16 -0500

    updating it on 4-21-2025

--- src/Store/ChatContext.tsx
+++ src/Store/ChatContext.tsx
@@ -1,122 +1,160 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, ReactNode } from "react";
 
-export const ChatContext = createContext({
-user:{},
-users:null,
-isMessage:false,
-selectedUser:null,
-addUser:()=>{},
-addUsers:()=>{},
-handleSelectedUser :()=>{},
-saveMessages:()=>{},
-});
-
-const handleStateAndData = (state, action) => {
-    if (action.type === "ADD-USER") {
-        localStorage.setItem("firstName",action.userDetails?.first_name );
-        localStorage.setItem("lastName",action.userDetails?.last_name );
-        localStorage.setItem("email",action.userDetails?.email);
-        localStorage.setItem("user_uuid" , action.userDetails?.user_uuid);
-        return {
-            ...state,
-            user:{
-                firstName: action.userDetails?.first_name ,
-            lastName: action.userDetails?.last_name,
-            email: action.userDetails?.email,
-            user_uuid:  action.userDetails?.user_uuid,
-            }
-        }
-    }
-
-    if (action.type==="ADD-USERS"){
-      return{
-        ...state,
-        users:action.usersDetails,
-      }
-    }
-    if (action.type==='SELECTED-USER'){
-        return{
-            ...state,
-            selectedUser:action.userSelected,
-        }
-    }
-    if (action.type==="IS-MESSAGE"){
-        return{
-            ...state,
-            isMessage:action.messagesData,
-        }
-    }
-
-    else{
-        return {
-            state,
-        }
-    }
+// Define types for the state and actions
+interface User {
+    firstName: string | null;
+    lastName: string | null;
+    email: string | null;
+    user_uuid: string | null;
 }
 
-export function ProjectContext({ children }) {
+interface StateType {
+    user: User;
+    users: User[] | null;
+    selectedUser: User | null;
+    isMessage: boolean;
+}
+
+interface ActionType {
+    type: string;
+    userDetails?: User;
+    usersDetails?: User[];
+    userSelected?: User | null;
+    messagesData?: boolean;
+}
+
+// Define the context type
+interface ChatContextType {
+    user: StateType["user"];
+    users: StateType["users"];
+    selectedUser: StateType["selectedUser"];
+    isMessage: boolean;
+    addUser: (userData: User) => void;
+    addUsers: (usersData: User[]) => void;
+    handleSelectedUser: (user: User | null) => void;
+    saveMessages: () => void;
+}
+
+// Create the context with proper typing
+export const ChatContext = createContext<ChatContextType>({
+    user: {
+        firstName: null,
+        lastName: null,
+        email: null,
+        user_uuid: null,
+    },
+    users: null,
+    selectedUser: null,
+    isMessage: false,
+    addUser: () => { },
+    addUsers: () => { },
+    handleSelectedUser: () => { },
+    saveMessages: () => { },
+});
+
+// Reducer function with proper typing
+const handleStateAndData = (state: StateType, action: ActionType): StateType => {
+    switch (action.type) {
+        case "ADD-USER":
+            if (action.userDetails) {
+                localStorage.setItem("firstName", action.userDetails.firstName || "");
+                localStorage.setItem("lastName", action.userDetails.lastName || "");
+                localStorage.setItem("email", action.userDetails.email || "");
+                localStorage.setItem("user_uuid", action.userDetails.user_uuid || "");
+            }
+            return {
+                ...state,
+                user: {
+                    firstName: action.userDetails?.firstName || null,
+                    lastName: action.userDetails?.lastName || null,
+                    email: action.userDetails?.email || null,
+                    user_uuid: action.userDetails?.user_uuid || null,
+                },
+            };
+
+        case "ADD-USERS":
+            return {
+                ...state,
+                users: action.usersDetails || null,
+            };
+
+        case "SELECTED-USER":
+            return {
+                ...state,
+                selectedUser: action.userSelected || null,
+            };
+
+        case "IS-MESSAGE":
+            return {
+                ...state,
+                isMessage: action.messagesData || false,
+            };
+
+        default:
+            return state;
+    }
+};
+
+// Define props for the ProjectContext component
+interface ProjectContextProps {
+    children: ReactNode;
+}
+
+export function ProjectContext({ children }: ProjectContextProps) {
     const [projectState, projectDispatchFunction] = useReducer(handleStateAndData, {
         user: {
             firstName: null,
-            lastName:null,
+            lastName: null,
             email: null,
-            user_uuid:null,
+            user_uuid: null,
         },
-        users:null,
-        selectedUser:null,
-        isMessage:false,
-    })
+        users: null,
+        selectedUser: null,
+        isMessage: false,
+    });
 
-    console.log(projectState.isMessage);
-
-
-    const addUser = (userData) => {        
+    const addUser = (userData: User) => {
         projectDispatchFunction({
             type: "ADD-USER",
-            userDetails: userData
-        })
-    }
+            userDetails: userData,
+        });
+    };
 
-    const addUsers = (usersData) =>{
+    const addUsers = (usersData: User[]) => {
         projectDispatchFunction({
-            type:"ADD-USERS",
-            usersDetails:usersData
-        })
-    }
+            type: "ADD-USERS",
+            usersDetails: usersData,
+        });
+    };
 
-    const handleSelectedUser = (user_uuid) =>{
+    const handleSelectedUser = (user: User | null) => {
         projectDispatchFunction({
-            type:"SELECTED-USER",
-            userSelected:user_uuid,
-        })
-    }
+            type: "SELECTED-USER",
+            userSelected: user,
+        });
+    };
 
-    const saveMessages = () =>{
+    const saveMessages = () => {
         projectDispatchFunction({
-            type:"IS-MESSAGE",
-            messagesData:true
-        })
-    }
-    const projectData = {
+            type: "IS-MESSAGE",
+            messagesData: true,
+        });
+    };
+
+    const projectData: ChatContextType = {
         user: projectState.user,
-        users:projectState.users,
-        selectedUser:projectState.selectedUser,
-        isMessage:projectState.isMessage,
+        users: projectState.users,
+        selectedUser: projectState.selectedUser,
+        isMessage: projectState.isMessage,
         addUser,
         addUsers,
         handleSelectedUser,
         saveMessages,
-    }
+    };
 
     return (
-        <>
-            <ChatContext.Provider value={projectData}>{children}</ChatContext.Provider>
-        </>
-    )
+        <ChatContext.Provider value={projectData}>{children}</ChatContext.Provider>
+    );
 }
 
 export default ChatContext;
-
-
-
-
